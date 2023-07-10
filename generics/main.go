8743f6784551f749911885e49e715b5d9fda4e55
Shibbaz <kmosc@protonmail.com>
2023-07-10 23:37:27 +0200

    feat: generic

--- generics/main.go
+++ generics/main.go
@@ -1,16 +1,38 @@
 package main
+
 import(
 	"fmt"
 )
 
+type Bank struct{
+	Name string
+	NetWorth int
+	Addr string
+}
+
+type Market[T Account] struct{
+	Name string
+	Volume int64
+	Hash string
+	transactions map[string]Transaction[T, T]
+}
+
+func (market *Market[Account]) Transactions()(*map[string]Transaction[Account, Account]){
+	return &market.transactions
+}
+
+
 type BankingAccount struct{
-	bankAddr string
-	amount	int
+	Id string
+	Bank *Bank
+	Amount	int
+	Market *Market[*BankingAccount]
 }
 
 type CryptoAccount struct {
-	cryptoAddr string
-	amount int
+	CryptoAddr string
+	Amount int
+	Market *Market[*CryptoAccount]
 }
 
 type Account interface{
@@ -18,36 +40,43 @@ type Account interface{
 }
 
 type Transaction[A Account, B Account] struct{
-	from A
-	to B
-	amount int
-	currency string
+	Source A
+	Direction B
+	Amount int
+	Currency string
 }
 
-func NewTransaction[A Account, B Account](from A, to B, amount int, currency string) *Transaction[A, B]{
-	return &Transaction[A, B]{
-		from: from,
-		to: to,
-		amount: amount,
-		currency: currency,
+func NewTransaction[A Account, B Account](Source A, Direction B, Amount int, Currency string) Transaction[A, B]{
+	return Transaction[A, B]{
+		Source: Source,
+		Direction: Direction,
+		Amount: Amount,
+		Currency: Currency,
 	}
 }
 
 func main(){
-	bank := BankingAccount{
-		bankAddr: "XD",
-		amount: 100,
+
+	market := Market[*CryptoAccount]{
+		Name: "Shibba",
+		Volume: 1092222222222222222,
+		Hash: "X827273ghjsgjdsgjsgks",
 	}
-	
-	crypto := CryptoAccount{
-		cryptoAddr: "YD",
-		amount: 200,
+
+	A := CryptoAccount{
+		CryptoAddr: "YD",
+		Amount: 200,
+		Market: &market,
 	}
-	transaction := Transaction[*BankingAccount, *CryptoAccount]{
-		from: &bank,
-		to: &crypto,
-		amount: 100,
-		currency: "usd",
+
+	B := CryptoAccount{
+		CryptoAddr: "YD",
+		Amount: 200,
+		Market: &market,
 	}
-	fmt.Println(transaction.currency)
+
+	market.transactions = map[string]Transaction[*CryptoAccount, *CryptoAccount]{
+		"0": NewTransaction[*CryptoAccount, *CryptoAccount](&A, &B, 100, "usd"),
+	}
+	fmt.Println(market.Transactions())
 }
