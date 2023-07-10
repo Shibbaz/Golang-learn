86967f593df4b909e4a163b24520575455b911fd
Shibbaz <kmosc@protonmail.com>
2023-07-10 22:29:39 +0200

    feat: generics

--- generics/main.go
+++ generics/main.go
@@ -1,31 +1,53 @@
 package main
-
-import (
+import(
 	"fmt"
-	. "libs"
 )
 
-func main() {
-	fmt.Println("Generics\n")
-	router := Router{
-		BaseAddr: "xd",
-	}
-	router.SetBaseAddr("localhost")
-	fmt.Println("new baseaddr ", router.GetBaseAddr())
-	routerGateway := Gateway[*Router]{
-		Data: &router,
-	}
-	Print(routerGateway.Reflect())
-
-	fmt.Println("\nApp example!")
-	app := App{
-		Port: 8080,
-	}
-	app.SetPort(9000)
-	fmt.Println("new port ", app.GetPort())
-	appGateway := Gateway[*App]{
-		Data: &app,
-	}
-	Print(appGateway.Reflect())
+type BankingAccount struct{
+	bankAddr string
+	amount	int
 }
 
+type CryptoAccount struct {
+	cryptoAddr string
+	amount int
+}
+
+type Account interface{
+	*BankingAccount | *CryptoAccount
+}
+
+type Transaction[A Account, B Account] struct{
+	from A
+	to B
+	amount int
+	currency string
+}
+
+func NewTransaction[A Account, B Account](from A, to B, amount int, currency string) *Transaction[A, B]{
+	return &Transaction[A, B]{
+		from: from,
+		to: to,
+		amount: amount,
+		currency: currency,
+	}
+}
+
+func main(){
+	bank := BankingAccount{
+		bankAddr: "XD",
+		amount: 100,
+	}
+	
+	crypto := CryptoAccount{
+		cryptoAddr: "YD",
+		amount: 200,
+	}
+	transaction := Transaction[*BankingAccount, *CryptoAccount]{
+		from: &bank,
+		to: &crypto,
+		amount: 100,
+		currency: "usd",
+	}
+	fmt.Println(transaction.currency)
+}
