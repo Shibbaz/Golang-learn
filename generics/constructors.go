71a624890df4e9be0e5b4a6622001609515485f7
Shibbaz <kmosc@protonmail.com>
2023-07-13 12:37:03 +0200

    style: move generics to new file

---
+++ generics/constructors.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+    "github.com/google/uuid"
+)
+
+func NewCryptoAccount(amount int) *CryptoAccount{
+	id := uuid.New()
+
+	A := CryptoAccount{
+		CryptoAddr: id.String(),
+		Amount: amount,
+		Market: &cryptoXMarket,
+	}
+	return &A
+}
+
+func NewBankingAccount(amount int) *BankingAccount{
+	id := uuid.New()
+
+	A := BankingAccount{
+		Id: id.String(),
+		Amount: amount,
+		Market: &bankXMarket,
+	}
+	return &A
+}
