92ca644d501611fe61a4c09a470b47830e9650b8
Shibbaz <kmosc@protonmail.com>
2023-07-26 10:49:11 +0200

    test: comments tests

--- grpc/tests/servers/server_comments_test.go
+++ grpc/tests/servers/server_comments_test.go
@@ -2,6 +2,8 @@ package servers
 
 import (
 	"context"
+	"fmt"
+	prisma "prisma_client"
 	"testing"
 
 	. "comments"
@@ -70,6 +72,92 @@ func TestCreateComment(t *testing.T) {
 				panic(err)
 			}
 			if tt.expected.out.Message != "[{id:"+id+" text:kamil44}]" {
+				t.Errorf("Out -> \nWant: %q\nGot: %q", tt.expected.out, comment)
+			}
+		})
+	}
+}
+
+func TestDeleteComment(t *testing.T) {
+	ctx := context.Background()
+	client, closer := NewCommentsTestServer(ctx)
+	defer closer()
+
+	comment, _ := prisma.New(nil).CreateComment(prisma.CommentCreateInput{}).Exec(ctx)
+
+	type expectation struct {
+		out *CommentsReply
+		err error
+	}
+	message := fmt.Sprintf("Deleted comment %s", comment.ID)
+
+	tests := map[string]struct {
+		in       *CommentArgs
+		expected expectation
+	}{
+		"Must_Success": {
+			in: &CommentArgs{
+				Id: comment.ID,
+			},
+			expected: expectation{
+				out: &CommentsReply{
+					Message: message,
+				},
+				err: nil,
+			},
+		},
+	}
+
+	for scenario, tt := range tests {
+		t.Run(scenario, func(t *testing.T) {
+			_, err := client.DeleteComment(ctx, tt.in)
+			if err != nil {
+				panic(err)
+			}
+			if tt.expected.out.Message != message {
+				t.Errorf("Out -> \nWant: %q\nGot: %q", tt.expected.out, message)
+			}
+		})
+	}
+}
+
+func TestUpdateComment(t *testing.T) {
+	ctx := context.Background()
+
+	client, closer := NewCommentsTestServer(ctx)
+	defer closer()
+
+	type expectation struct {
+		out *CommentsReply
+		err error
+	}
+	comment, _ := prisma.New(nil).CreateComment(prisma.CommentCreateInput{}).Exec(ctx)
+	CommentId := comment.ID
+	tests := map[string]struct {
+		in       *CommentArgs
+		expected expectation
+	}{
+		"Must_Success": {
+			in: &CommentArgs{
+				Id:   CommentId,
+				Text: "Kamilsd",
+			},
+			expected: expectation{
+				out: &CommentsReply{
+					Message: "[{ID:" + CommentId + " Title:Kamilsd}]",
+				},
+				err: nil,
+			},
+		},
+	}
+
+	for scenario, tt := range tests {
+		t.Run(scenario, func(t *testing.T) {
+			comment, err := client.UpdateComment(ctx, tt.in)
+			if err != nil {
+				panic(err)
+			}
+			if tt.expected.out.Message != "[{ID:"+CommentId+" Text:Kamilsd}]" {
 				t.Errorf("Out -> \nWant: %q\nGot: %q", tt.expected.out, comment)
 			}
 		})
